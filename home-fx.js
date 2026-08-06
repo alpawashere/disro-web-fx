84520800558c1859df42aa78de24089aab2565de
alpawashere <alessandro@whitestairs.studio>
2026-08-06 00:36:31 -0400

    Route Home-WIP alternate hero to staging branch

--- home-fx.js
+++ home-fx.js
@@ -6,13 +6,17 @@
   var PROD_BASE = 'https://raw.githubusercontent.com/alpawashere/disro-web-fx/main/home/';
   var STAGING_BASE = 'https://raw.githubusercontent.com/alpawashere/disro-web-fx/staging/ask-cortex-v2/home/';
   var SLACK_BASE = 'https://raw.githubusercontent.com/alpawashere/disro-web-fx/staging/slack-adidas/home/';
+  var HERO_ALT_BASE = 'https://raw.githubusercontent.com/alpawashere/disro-web-fx/staging/home-wip-hero/home/';
   var isStaging = /\.webflow\.io$/.test(window.location.hostname);
+  var isHomeWip = window.location.pathname.replace(/\/+$/, '') === '/home-wip';
+  var useAltHero = isStaging && isHomeWip && new URLSearchParams(window.location.search).get('hero') === 'alt';
   var files = ['fonts.js', 'organigram.js', 'shine.js', 'slack.js', 'cortex.js', 'loop2-mobile.js', 'dmocks.js', 'debug.js', 'hero-new.js'];
 
   files.forEach(function (f) {
     var base = PROD_BASE;
     if (isStaging && f === 'cortex.js') base = STAGING_BASE;
     if (isStaging && f === 'slack.js') base = SLACK_BASE;
+    if (useAltHero && f === 'hero-new.js') base = HERO_ALT_BASE;
     fetch(base + f + '?cb=' + Date.now()).then(function (r) { return r.text(); }).then(function (t) {
       var s = document.createElement('script');
       s.textContent = t;
