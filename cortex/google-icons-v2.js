195c68d739464ae4251a017a66200237c9c67913
alpawashere <alessandro@whitestairs.studio>
2026-08-06 08:08:21 -0400

    Add scoped Cortex Google icon mapping

---
+++ cortex/google-icons-v2.js
@@ -0,0 +1,14 @@
+/* Cortex Google file marks — official product icons only for matching items
+   inside the Meadowlark Brand Assets slide. */
+(function () {
+  var css = document.createElement('style');
+  css.setAttribute('data-cortex-google-file-marks-v2', 'true');
+  css.textContent =
+    '.cgf-grid .cgf-item:nth-child(1) .cgf-thumb,.cgf-grid .cgf-item:nth-child(3) .cgf-thumb,.cgf-grid .cgf-item:nth-child(4) .cgf-thumb,.cgf-grid .cgf-item:nth-child(5) .cgf-thumb{background-repeat:no-repeat;background-position:center;background-size:30px 30px}' +
+    '.cgf-grid .cgf-item:nth-child(1) .cgf-thumb>svg,.cgf-grid .cgf-item:nth-child(3) .cgf-thumb>svg,.cgf-grid .cgf-item:nth-child(4) .cgf-thumb>svg,.cgf-grid .cgf-item:nth-child(5) .cgf-thumb>svg{display:none}' +
+    '.cgf-grid .cgf-item:nth-child(1) .cgf-thumb{background-image:url("https://cdn.prod.website-files.com/698752f9145b6f03fa98b16d/6a7254c911be987b92036202_192px%20(1).svg")}' +
+    '.cgf-grid .cgf-item:nth-child(3) .cgf-thumb{background-image:url("https://cdn.prod.website-files.com/698752f9145b6f03fa98b16d/6a72323343bb54d5df081401_connector-google-drive.svg")}' +
+    '.cgf-grid .cgf-item:nth-child(4) .cgf-thumb{background-image:url("https://cdn.prod.website-files.com/698752f9145b6f03fa98b16d/6a7254c9a58721f547ee750a_192px%20(2).svg")}' +
+    '.cgf-grid .cgf-item:nth-child(5) .cgf-thumb{background-image:url("https://cdn.prod.website-files.com/698752f9145b6f03fa98b16d/6a7254c93f1c1a31b0992130_192px.svg")}';
+  document.head.appendChild(css);
+})();
